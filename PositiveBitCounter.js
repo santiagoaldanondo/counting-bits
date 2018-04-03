492735fc7f68acd597f86188f2fdbc35c2b1f1ba
santiagoaldanondo <santiagoaldanondo@gmail.com>
2018-04-03 17:54:44 +0200

    style fixes

--- PositiveBitCounter.js
+++ PositiveBitCounter.js
@@ -1,18 +1,13 @@
-function Count(input) {
-  if (!Number.isInteger(input)) // Extra validation to check if input is an integer
-  { 
-    throw new Error('input is not an integer') 
-  } 
-  else if (input < 0) 
-  {
+function Count (input) {
+  if (!Number.isInteger(input)) { // Extra validation to check if input is an integer
+    throw new Error('input is not an integer')
+  } else if (input < 0) {
     throw new RangeError('input is negative')
-  } 
-  else 
-  {
+  } else {
     let count = 0
     let result = []
-    let binArray = input.toString(2).split("").reverse() // Convert to binary, array and reverse
-    for (bit in binArray) {
+    let binArray = input.toString(2).split('').reverse() // Convert to binary, array and reverse
+    for (let bit in binArray) {
       let bitValue = parseInt(binArray[bit])
       count += bitValue
       if (bitValue === 1) {
@@ -20,7 +15,7 @@ function Count(input) {
       }
     }
     result.unshift(count) // count should be index[0] of the result
-    return result      
+    return result
   }
 }
 
--- PositiveBitCounter.test.js
+++ PositiveBitCounter.test.js
@@ -2,11 +2,10 @@ const PositiveBitCounter = require('./PositiveBitCounter')
 const assert = require('assert')
 
 describe('PositiveBitCounter', function () {
-
   // Extra validation to check if input is an integer
   it('Should return an Error when the supplied value is not an Integer', function () {
     assert.throws(
-      () => PositiveBitCounter.Count("a"),
+      () => PositiveBitCounter.Count('a'),
       function isError (error) {
         return (error instanceof Error)
       })
